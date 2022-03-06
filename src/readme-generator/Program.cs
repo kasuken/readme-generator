93a54998a1511b7daec857dbb75ee2e865487a4e
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-03-06 15:41:31 +0100

    first tests

---
+++ src/readme-generator/Program.cs
@@ -0,0 +1,54 @@
+﻿using Spectre.Console;
+
+var answers = new AnswersModel();
+
+AnsiConsole.Write(
+ new FigletText("readme-generator")
+     .Centered()
+     .Color(Color.White));
+
+
+answers.ProjectName = AnsiConsole.Prompt<string>(
+    new TextPrompt<string>("Enter the [green]project name[/]?")
+    );
+
+answers.License = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Select [green]license[/] to use")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more license)[/]")
+        .AddChoices(new[] {
+            "MIT", "Apache","GPL"
+        }));
+
+answers.projetLogoYesNo = AnsiConsole.Confirm("Do you have a project logo?");
+
+if (answers.projetLogoYesNo)
+{
+    answers.ProjectLogo = AnsiConsole.Prompt<string>(
+        new TextPrompt<string>("Enter the image url of the project logo (leave blank if none)")
+        );
+}
+
+answers.ShortDescription = AnsiConsole.Prompt<string>(
+    new TextPrompt<string>("Enter the short description (It should be short, concise to hook the reader)")
+    );
+
+answers.ImageYesNo = AnsiConsole.Confirm("Do you have an image for the project?");
+
+if (answers.ImageYesNo)
+{
+    answers.ImageUrl = AnsiConsole.Prompt<string>(
+        new TextPrompt<string>("Enter the image url (leave blank if none)")
+        );
+}
+
+answers.DemoImageYesNo = AnsiConsole.Confirm("Do you have an image/gif for the demo of the project?");
+
+if (answers.DemoImageYesNo)
+{
+    answers.DemoImageUrl = AnsiConsole.Prompt<string>(
+        new TextPrompt<string>("Enter the demo image url (leave blank if none)")
+        );
+}
+
---
+++ src/readme-generator/QuestionsModel.cs
@@ -0,0 +1,20 @@
+﻿public class AnswersModel
+{
+    public string ProjectName { get; set; }
+
+    public string License { get; set; }
+
+    public bool projetLogoYesNo { get; set; }
+
+    public string ProjectLogo { get; set; }
+
+    public string ShortDescription { get; set; }
+
+    public bool ImageYesNo { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public bool DemoImageYesNo { get; set; }
+
+    public string DemoImageUrl { get; set; }
+}
