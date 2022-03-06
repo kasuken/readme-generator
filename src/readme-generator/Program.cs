33786c0288fdc6c5adfabcfa9bf055d5cb819f73
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-03-06 21:20:26 +0100

    release 1.1.0

--- src/readme-generator/Program.cs
+++ src/readme-generator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Spectre.Console;
+using System.Text;
 
 var answers = new AnswersModel();
 
@@ -54,9 +55,71 @@ if (answers.DemoImageYesNo)
 answers.ContributingYesNo = AnsiConsole.Confirm("Do you want to add a Contributing section?");
 
 AnsiConsole.Status()
-    .Start("Generating README...", ctx =>
+    .Start("Generating README.md...", ctx =>
     {
-        string readmePath = Directory.GetCurrentDirectory();
+        var readmePath = Path.Combine(Directory.GetCurrentDirectory(), "README.md");
+        var sb = new StringBuilder();
 
+        sb.AppendLine("<div align=\"center\">");
+        sb.AppendLine($"<h1>{answers.ProjectName}</h1>");
 
+        if (answers.projectLogoYesNo)
+        {
+            sb.AppendLine($"<img src =\"{answers.ProjectLogo}\" alt=\"{answers.ProjectName}\" align=\"center\" width=\"100\" height=\"100\">");
+        }
+
+        sb.AppendLine("<br />");
+
+        switch (answers.License.ToLower())
+        {
+            case "mit":
+                sb.AppendLine("<img alt=\"License: MIT\" src=\"https://img.shields.io/badge/License-MIT-blue.svg\" />");
+                break;
+            case "apache":
+                sb.AppendLine("<img alt=\"License: Apache\" src=\"https://img.shields.io/badge/license-Apache%202-blue\" />");
+                break;
+            case "gpl":
+                sb.AppendLine("<img alt=\"License: GPL\" src=\"https://img.shields.io/badge/license-GPL-blue\" />");
+                break;
+
+            default:
+                break;
+        }
+
+        sb.AppendLine("<br />");
+
+        sb.AppendLine($"{answers.ShortDescription}");
+
+        sb.AppendLine("<br />");
+
+        if (answers.ImageYesNo)
+        {
+            sb.AppendLine("<hr />");
+            sb.AppendLine($"<h2>Screenshots</h2>");
+            sb.AppendLine($"<img src =\"{answers.ImageUrl}\" alt=\"{answers.ProjectName}\" align=\"center\" height=\"500\">");
+        }
+
+        sb.AppendLine("<br />");
+
+        if (answers.DemoImageYesNo)
+        {
+            sb.AppendLine("<hr />");
+            sb.AppendLine($"<h2>Demo</h2>");
+            sb.AppendLine($"<img src =\"{answers.DemoImageUrl}\" alt=\"{answers.ProjectName}\" align=\"center\" height=\"500\">");
+        }
+
+        sb.AppendLine("<br />");
+
+        if (answers.ImageYesNo)
+        {
+            sb.AppendLine("<hr />");
+            sb.AppendLine($"<h2>Contributing</h2>");
+            sb.AppendLine("Contributions are always welcome!");
+        }
+
+        sb.AppendLine("</div>");
+
+        File.WriteAllText(readmePath, sb.ToString());
+
+        AnsiConsole.MarkupLine($"README created successfully: {readmePath}");
     });
